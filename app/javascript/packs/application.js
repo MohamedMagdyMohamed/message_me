975cb708e8e5f4c2a5a7dc4e1ab0bad6fb373f26
Mohamed Magdy Mohamed <mm.medany96@gmail.com>
2019-11-21 00:15:13 +0200

    Add semantic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// loads jquery
+//= require jquery
+
+// Loads all Semantic javascripts
+//= require semantic-ui
