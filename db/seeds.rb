9fef537c86dfafa6aa782f32f61f02e15bcd2ee7
Mohamed Magdy Mohamed <mm.medany96@gmail.com>
2019-11-22 14:31:31 +0200

    Add users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Magdy", password: "password")
+User.create(username: "Medany", password: "password")
+User.create(username: "Omar", password: "password")
+User.create(username: "Mostafa", password: "password")
+User.create(username: "Ahmed", password: "password")
